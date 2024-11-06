75d5dcf557fc7ac595f1e535fa22c6d5a3fb11e9
Саша Корнієнко <iqura@Sasas-MacBook-Pro.local>
2024-11-06 20:48:40 +0200

    chore: changed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,8 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Theme, ThemeProvider } from "@react-navigation/native";
 import { PortalHost } from "@rn-primitives/portal";
 import { SplashScreen, Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
 import * as React from "react";
-import { Platform } from "react-native";
-import { ThemeToggle } from "~/components/ThemeToggle";
-import "~/global.css"; // Ensure this path is correct and the file exists
-import { setAndroidNavigationBar } from "~/lib/android-navigation-bar";
-import { NAV_THEME } from "~/lib/constants";
-import { useColorScheme } from "~/lib/useColorScheme";
+import "~/global.css";
 
-const LIGHT_THEME: Theme = {
-  dark: false,
-  colors: NAV_THEME.light,
-};
-
-const DARK_THEME: Theme = {
-  dark: true,
-  colors: NAV_THEME.dark,
-};
 
 export { ErrorBoundary } from "expo-router";
 
@@ -27,65 +10,25 @@ export { ErrorBoundary } from "expo-router";
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const { colorScheme, setColorScheme, isDarkColorScheme } = useColorScheme();
-  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
-
-  React.useEffect(() => {
-    (async () => {
-      try {
-        const theme = await AsyncStorage.getItem("theme");
-
-        if (Platform.OS === "web") {
-          // Adds the background color to the html element to prevent white background on overscroll.
-          document.documentElement.classList.add("bg-background");
-        }
-
-        if (!theme) {
-          await AsyncStorage.setItem("theme", colorScheme);
-          setIsColorSchemeLoaded(true);
-          return;
-        }
-
-        const colorTheme = theme === "dark" ? "dark" : "light";
-        if (colorTheme !== colorScheme) {
-          setColorScheme(colorTheme);
-        }
-
-        setAndroidNavigationBar(colorTheme);
-        setIsColorSchemeLoaded(true);
-      } catch (error) {
-        console.error("Error loading theme:", error);
-      }
-    })().finally(() => {
-      SplashScreen.hideAsync();
-    });
-  }, [colorScheme, setColorScheme]);
-
-  if (!isColorSchemeLoaded) {
-    return null; // Show nothing while loading the color scheme
-  }
-
   return (
-    <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
-      <StatusBar style={isDarkColorScheme ? "light" : "dark"} />
+   <>
       <Stack>
         <Stack.Screen
           name="index"
           options={{
             title: "Lab 1, made by Korniienko Oleksandr",
-            headerRight: () => <ThemeToggle />,
           }}
         />
         <Stack.Screen
           name="aboutMe"
           options={{
             title: "About Me",
-            headerRight: () => <ThemeToggle />,
+     
             headerBackTitleVisible: false,
           }}
         />
       </Stack>
       <PortalHost />
-    </ThemeProvider>
+      </>
   );
 }
